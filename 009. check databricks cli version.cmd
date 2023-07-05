096ad265738390ac94a5434c511ed31bb054a907
Davide Moraschi <davidem@eurostrategy.net>
2023-07-05 12:33:48 +0200

    list clusters

---
+++ 009. check databricks cli version.cmd
@@ -0,0 +1,1 @@
+C:\databricks_cli_0.200.1_windows_amd64\databricks.exe version
---
+++ 010. list clusters in a databricks workspace.cmd
@@ -0,0 +1,5 @@
+rem set DATABRICKS_HOST environment variable, example: 
+rem set DATABRICKS_HOST=https://adb-************.4.azuredatabricks.net
+rem $env:DATABRICKS_HOST = 'https://adb-*************4.4.azuredatabricks.net'
+
+C:\databricks_cli_0.200.1_windows_amd64\databricks.exe clusters list
